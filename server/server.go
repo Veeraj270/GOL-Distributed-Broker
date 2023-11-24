c65dbe04eb73e695aea40190bccc547957075f43
Veeraj270 <123737105+Veeraj270@users.noreply.github.com>
2023-11-24 02:55:27 +0000

    Add files via upload

--- server/server.go
+++ server/server.go
@@ -221,14 +221,14 @@ func remoteDistributor(world [][]uint8, turns int, threads int) [][]uint8 {
 	clients = make([]*rpc.Client, threads)
 	errs := make([]error, threads)
 	address := make([]string, threads)
-	address[0] = "54.242.253.12:3040"
-	address[1] = "34.229.159.250:3040"
-	address[2] = "52.23.230.155:3040"
-	address[3] = "54.162.208.37:3040"
-	address[4] = "34.224.78.220:3040"
-	address[5] = "54.226.16.66:3040"
-	address[6] = "34.227.195.170:3040"
-	address[7] = "50.19.31.194:3040"
+	address[0] = "54.242.253.12:8040"
+	address[1] = "34.229.159.250:8040"
+	address[2] = "52.23.230.155:8040"
+	address[3] = "54.162.208.37:8040"
+	address[4] = "34.224.78.220:8040"
+	address[5] = "54.226.16.66:8040"
+	address[6] = "34.227.195.170:8040"
+	address[7] = "50.19.31.194:8040"
 
 	for i := 0; i < threads; i++ {
 
